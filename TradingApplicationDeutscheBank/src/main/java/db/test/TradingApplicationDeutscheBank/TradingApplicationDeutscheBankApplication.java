6d13d923624dd5fffd0074ca21d5e46f3f217516
Prathap <prathapdhnskrn19@gmail.com>
2023-10-19 13:08:54 +0200

    Base Springboot application setup for Trading application Deutsche bank

---
+++ TradingApplicationDeutscheBank/src/main/java/db/test/TradingApplicationDeutscheBank/TradingApplicationDeutscheBankApplication.java
@@ -0,0 +1,13 @@
+package db.test.TradingApplicationDeutscheBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingApplicationDeutscheBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingApplicationDeutscheBankApplication.class, args);
+	}
+
+}
---
+++ TradingApplicationDeutscheBank/src/test/java/db/test/TradingApplicationDeutscheBank/TradingApplicationDeutscheBankApplicationTests.java
@@ -0,0 +1,13 @@
+package db.test.TradingApplicationDeutscheBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingApplicationDeutscheBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
